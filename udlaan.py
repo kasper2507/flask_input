e6ab17cd03a73a1425c4e4220f8b1d843ac65bfc
Kasper Stounberg <kasper2507@gmail.com>
2022-04-01 19:19:46 +0200

    Delete udlaan.py

--- udlaan.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-
-class UdlaanForm(FlaskForm):
-    customerID = StringField('Customer ID')
-    inventoryID = StringField('Inventory ID')
-    staffID = StringField('Staff ID')
-    amount = StringField('amount')
-    submit = SubmitField('Submit')
